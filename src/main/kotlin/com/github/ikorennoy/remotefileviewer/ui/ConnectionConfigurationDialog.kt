73a840fe51131efdfe8796e526145bf0952e7b82
Ilya Korennoy <korennoy.ilya@gmail.com>
2023-10-31 13:28:01 +0100

    Save connection configuration state

--- src/main/kotlin/com/github/ikorennoy/remotefileviewer/ui/ConnectionConfigurationDialog.kt
+++ src/main/kotlin/com/github/ikorennoy/remotefileviewer/ui/ConnectionConfigurationDialog.kt
@@ -5,6 +5,7 @@ import com.github.ikorennoy.remotefileviewer.template.FileViewerBundle
 import com.intellij.openapi.application.EDT
 import com.intellij.openapi.application.ModalityState
 import com.intellij.openapi.application.asContextElement
+import com.intellij.openapi.components.*
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.openapi.ui.ValidationInfo
@@ -16,22 +17,29 @@ import com.intellij.ui.components.JBPasswordField
 import com.intellij.ui.components.JBTextField
 import com.intellij.ui.components.fields.ExtendableTextComponent
 import com.intellij.ui.components.fields.ExtendableTextField
-import com.intellij.ui.dsl.builder.COLUMNS_SHORT
-import com.intellij.ui.dsl.builder.bindIntText
-import com.intellij.ui.dsl.builder.panel
-import com.intellij.ui.dsl.builder.text
+import com.intellij.ui.dsl.builder.*
 import kotlinx.coroutines.*
 import javax.swing.JComponent
 import javax.swing.event.DocumentEvent
 
 class ConnectionConfigurationDialog(project: Project, private val remoteFs: SftpFileSystem) : DialogWrapper(project) {
 
-
     private val DEFAULT_PORT = 22
+
+    private val sshConfiguration = service<SshConnectionConfiguration>()
 
     private val hostField = ExtendableTextField(COLUMNS_SHORT)
     private val usernameField = JBTextField(COLUMNS_SHORT)
     private val passwordField = JBPasswordField()
+    private val portField: JBTextField = JBTextField(COLUMNS_TINY)
+    private val rootField: JBTextField = JBTextField(COLUMNS_SHORT)
+
+    init {
+        hostField.text = sshConfiguration.host
+        usernameField.text = sshConfiguration.username
+        portField.text = sshConfiguration.port.toString()
+        rootField.text = sshConfiguration.root
+    }
 
 
     private val loadingExtension = ExtendableTextComponent.Extension { AnimatedIcon.Default.INSTANCE }
@@ -48,13 +56,23 @@ class ConnectionConfigurationDialog(project: Project, private val remoteFs: Sftp
     }
 
     var port: Int = DEFAULT_PORT
-    val host: String get() = hostField.text.orEmpty().trim()
-    val root: String get() = rootField.text.orEmpty().trim()
-    val username: String get() = usernameField.text.orEmpty().trim()
+    val host: String get() = sshConfiguration.host
+    val root: String get() = sshConfiguration.root
+    val username: String get() = sshConfiguration.username
     val password: CharArray get() = passwordField.password
-    lateinit var portField: JBTextField
-    lateinit var rootField: JBTextField
 
+
+    override fun getPreferredFocusedComponent(): JComponent {
+        return if (hostField.text.isEmpty()) {
+            hostField
+        } else if (portField.text.isEmpty()) {
+            portField
+        } else if (usernameField.text.isEmpty()) {
+            usernameField
+        } else {
+            passwordField
+        }
+    }
 
     override fun createCenterPanel(): JComponent = panel {
         row {
@@ -66,14 +84,12 @@ class ConnectionConfigurationDialog(project: Project, private val remoteFs: Sftp
                 .focused()
 
             label(FileViewerBundle.message("connection.configuration.dialog.port"))
-            portField = intTextField(0..65536)
-                .bindIntText(::port)
-                .text("22").component
+            cell(portField)
         }
         row {
             label(FileViewerBundle.message("connection.configuration.dialog.root"))
                 .widthGroup("CredentialsLabel")
-            rootField = textField().component
+            cell(rootField)
         }
         row {
             label(FileViewerBundle.message("connection.configuration.dialog.username"))
@@ -92,6 +108,7 @@ class ConnectionConfigurationDialog(project: Project, private val remoteFs: Sftp
     override fun doOKAction() {
         scope.launch(uiDispatcher) {
             setLoading(true)
+            saveState()
             try {
                 accessError = checkAccess()
             } finally {
@@ -110,6 +127,13 @@ class ConnectionConfigurationDialog(project: Project, private val remoteFs: Sftp
         }
     }
 
+    private fun saveState() {
+        sshConfiguration.host = hostField.text.trim()
+        sshConfiguration.port = portField.text.toInt()
+        sshConfiguration.root = rootField.text.trim()
+        sshConfiguration.username = usernameField.text.trim()
+    }
+
     private fun setLoading(isLoading: Boolean) {
         rootField.isEnabled = !isLoading
         portField.isEnabled = !isLoading
@@ -124,14 +148,14 @@ class ConnectionConfigurationDialog(project: Project, private val remoteFs: Sftp
     }
 
     private fun checkHostNotBlank(): ValidationInfo? =
-        if (host.isNotEmpty()) null
+        if (hostField.text.isNotEmpty()) null
         else ValidationInfo(
             FileViewerBundle.message("connection.configuration.dialog.host.empty.validation"),
             hostField
         )
 
     private fun checkUsernameNotBlank(): ValidationInfo? =
-        if (username.isNotEmpty()) null
+        if (usernameField.text.isNotEmpty()) null
         else ValidationInfo(
             FileViewerBundle.message("connection.configuration.dialog.username.empty.validation"),
             usernameField
@@ -171,4 +195,43 @@ class ConnectionConfigurationDialog(project: Project, private val remoteFs: Sftp
                 accessError = null
             }
         })
+}
+
+@Service
+@State(name = "SshConnectionConfiguration", storages = [Storage("SshConfiguration.xml")])
+class SshConnectionConfiguration :
+    SimplePersistentStateComponent<SshConnectionConfiguration.SshConnectionConfigurationState>(
+        SshConnectionConfigurationState()
+    ) {
+
+    var host: String
+        get() = state.host ?: ""
+        set(value) {
+            state.host = value
+        }
+
+    var port: Int
+        get() = state.port
+        set(value) {
+            state.port = value
+        }
+
+    var root: String
+        get() = state.root ?: ""
+        set(value) {
+            state.root = value
+        }
+
+    var username: String
+        get() = state.username ?: ""
+        set(value) {
+            state.username = value
+        }
+
+    class SshConnectionConfigurationState : BaseState() {
+        var host by string()
+        var port by property(22)
+        var root by string()
+        var username by string()
+    }
 }
